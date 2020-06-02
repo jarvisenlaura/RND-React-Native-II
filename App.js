689bbc606fb69b3fd7493831612be32cd2e8ccc4
Laura Järvinen <laurajarvinen@Lauras-MacBook-Pro.local>
2020-06-02 18:43:07 +0300

    apikutsu lisätty ja api-avain piilotettu

--- App.js
+++ App.js
@@ -6,8 +6,11 @@ import {
   View,
   TouchableHighlight,
   PermissionsAndroid,
+  ActivityIndicator,
 } from 'react-native';
 import CallDetectorManager from 'react-native-call-detection';
+import {apikey} from './keys';
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -15,6 +18,9 @@ export default class App extends React.Component {
       featureOn: false,
       incoming: false,
       number: null,
+      numberInfo: null,
+      loading: false,
+      error: null,
     };
   }
   componentDidMount() {
@@ -37,10 +43,17 @@ export default class App extends React.Component {
       (event, number) => {
         if (event === 'Disconnected') {
           // Do something call got disconnected
-          this.setState({incoming: false, number: null});
+          this.setState({
+            incoming: false,
+            number: null,
+            numberInfo: null,
+            error: null,
+            loading: false,
+          });
         } else if (event === 'Incoming') {
           // Do something call got incoming
           this.setState({incoming: true, number});
+          this.getNumberInfo(number);
         } else if (event === 'Offhook') {
           //Device call state: Off-hook.
           // At least one call exists that is dialing,
@@ -49,31 +62,77 @@ export default class App extends React.Component {
           this.setState({incoming: true, number});
         } else if (event === 'Missed') {
           // Do something call got missed
-          this.setState({incoming: false, number: null});
+          this.setState({
+            incoming: false,
+            number: null,
+            error: null,
+            loading: false,
+          });
         }
       },
       true, // if you want to read the phone number of the incoming call [ANDROID], otherwise false
-      () => {}, // callback if your permission got denied [ANDROID] [only if you want to read incoming number] default: console.error
+      () => {},
+      // callback if your permission got denied [ANDROID] [only if
+      //you want to read incoming number] default: console.error
       {
         title: 'Phone State Permission',
         message:
           'This app needs access to your phone state in order to react and/or to adapt to incoming calls.',
-      }, // a custom permission request message to explain to your user, why you need the permission [recommended] - this is the default one
+      }, // a custom permission request message to explain to your
+      //user, why you need the permission [recommended] - this
+      // is the default one
     );
   };
   stopListenerTapped = () => {
-    this.setState({featureOn: false});
+    this.setState({
+      featureOn: false,
+      incoming: false,
+      number: null,
+      numberInfo: null,
+      error: null,
+    });
     this.callDetector && this.callDetector.dispose();
   };
+  getNumberInfo = async (number) => {
+    try {
+      this.setState({loading: true});
+      const response = await fetch(
+        `https://api.vainu.io/api/v1/prospects/filter/?country=FI&phone=` +
+          number,
+        {
+          method: 'GET',
+          headers: {'API-key': apikey},
+        },
+      );
+      const info = await response.json();
+      // parses JSON response into native JavaScript objects
+      if (info && info[0] && this.state.incoming) {
+        this.setState({numberInfo: info[0], loading: false});
+      } else {
+        this.setState({numberInfo: [0], loading: false});
+      }
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: this.state.incoming ? error : null,
+      });
+    }
+  };
   render() {
+    const {
+      numberInfo,
+      incoming,
+      number,
+      loading,
+      error,
+      featureOn,
+    } = this.state;
     return (
-      <View style={styles.body}>
+      <View style={styles.container}>
         <Text style={styles.text}>Laitetaanko numerotutka päälle?</Text>
         <TouchableHighlight
           onPress={
-            this.state.featureOn
-              ? this.stopListenerTapped
-              : this.startListenerTapped
+            featureOn ? this.stopListenerTapped : this.startListenerTapped
           }>
           <View
             style={{
@@ -81,24 +140,25 @@ export default class App extends React.Component {
               height: 200,
               justifyContent: 'center',
               alignItems: 'center',
-              backgroundColor: this.state.featureOn
-                ? 'greenyellow'
-                : 'indianred',
+              backgroundColor: featureOn ? 'greenyellow' : 'indianred',
             }}>
             <Text style={styles.text}>
-              {this.state.featureOn ? `On päällä` : `On pois päältä`}{' '}
+              {featureOn ? `On päällä` : `On pois päältä`}{' '}
             </Text>
           </View>
         </TouchableHighlight>
-        {this.state.incoming && (
-          <Text style={{fontSize: 50}}>PUHELU {this.state.number}</Text>
+        {incoming && <Text style={{fontSize: 20}}>PUHELU {number}</Text>}
+        {loading && <Text style={{fontSize: 20}}>Ladataan tietoja</Text>}
+        {numberInfo && (
+          <Text style={{fontSize: 20}}>{JSON.stringify(numberInfo)}</Text>
         )}
+        {error && <Text>Tapahtui virhe</Text>}
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
-  body: {
+  container: {
     backgroundColor: 'honeydew',
     justifyContent: 'center',
     alignItems: 'center',
@@ -108,5 +168,4 @@ const styles = StyleSheet.create({
     padding: 20,
     fontSize: 20,
   },
-  button: {},
 });
