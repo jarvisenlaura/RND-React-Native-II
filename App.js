0e52e1fe4c686f3e493f43d23a2c10e84db543df
Laura Järvinen <laurajarvinen@Lauras-MacBook-Pro.local>
2020-06-05 13:51:21 +0300

    lisätty redux ja sagat

--- App.js
+++ App.js
@@ -6,67 +6,62 @@ import {
   View,
   TouchableHighlight,
   PermissionsAndroid,
+  ActivityIndicator,
 } from 'react-native';
+import {connect} from 'react-redux';
 import CallDetectorManager from 'react-native-call-detection';
-import {apikey} from './keys';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      featureOn: false,
-      incoming: false,
-      number: null,
-      numberInfo: null,
-      loading: false,
-      error: null,
-    };
-  }
+import {
+  setListening,
+  incomingCall,
+  isLoading,
+  getNumber,
+  getInfo,
+  isError,
+  noCall,
+} from './actions';
+
+class App extends React.Component {
   componentDidMount() {
     this.askPermission();
   }
+
   askPermission = async () => {
     try {
       const permissions = await PermissionsAndroid.requestMultiple([
         PermissionsAndroid.PERMISSIONS.READ_CALL_LOG,
         PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE,
       ]);
-      console.log('Permissions are:', permissions);
+      console.log(permissions);
     } catch (err) {
       console.warn(err);
     }
   };
+
   startListenerTapped = () => {
-    this.setState({featureOn: true});
+    const {incomingCall, getNumber, noCall, getInfo, setListening} = this.props;
+
+    setListening(true);
     this.callDetector = new CallDetectorManager(
       (event, number) => {
         if (event === 'Disconnected') {
           // Do something call got disconnected
-          this.setState({
-            incoming: false,
-            number: null,
-            numberInfo: null,
-            error: null,
-            loading: false,
-          });
+          noCall();
         } else if (event === 'Incoming') {
           // Do something call got incoming
-          this.setState({incoming: true, number});
-          this.getNumberInfo(number);
+          incomingCall(true);
+          getNumber(number);
+          getInfo(number);
         } else if (event === 'Offhook') {
           //Device call state: Off-hook.
           // At least one call exists that is dialing,
           // active, or on hold,
           // and no calls are ringing or waiting.
-          this.setState({incoming: true, number});
+          incomingCall(true);
+          getNumber(number);
         } else if (event === 'Missed') {
           // Do something call got missed
-          this.setState({
-            incoming: false,
-            number: null,
-            error: null,
-            loading: false,
-          });
+          noCall();
         }
       },
       true, // if you want to read the phone number of the incoming call [ANDROID], otherwise false
@@ -77,55 +72,49 @@ export default class App extends React.Component {
         title: 'Phone State Permission',
         message:
           'This app needs access to your phone state in order to react and/or to adapt to incoming calls.',
-      }, // a custom permission request message to explain to your
-      //user, why you need the permission [recommended] - this
-      // is the default one
+      },
     );
   };
+
   stopListenerTapped = () => {
-    this.setState({
-      featureOn: false,
-      incoming: false,
-      number: null,
-      numberInfo: null,
-      error: null,
-    });
+    const {noCall, setListening} = this.props;
+
+    setListening(false);
+    noCall();
     this.callDetector && this.callDetector.dispose();
   };
-  getNumberInfo = async (number) => {
-    try {
-      this.setState({loading: true});
-      const response = await fetch(
-        `https://api.vainu.io/api/v1/prospects/filter/?country=FI&phone=` +
-          number,
-        {
-          method: 'GET',
-          headers: {'API-key': apikey},
-        },
-      );
-      const info = await response.json();
-      // parses JSON response into native JavaScript objects
-      if (info && info[0] && this.state.incoming) {
-        this.setState({numberInfo: info[0], loading: false});
-      } else {
-        this.setState({numberInfo: [0], loading: false});
-      }
-    } catch (error) {
-      this.setState({
-        loading: false,
-        error: this.state.incoming ? error : null,
-      });
-    }
-  };
+
   render() {
     const {
-      numberInfo,
-      incoming,
-      number,
-      loading,
-      error,
       featureOn,
-    } = this.state;
+      incoming,
+      loading,
+      number,
+      numberInfo,
+      error,
+    } = this.props;
+    let result;
+
+    if (numberInfo && numberInfo.industry_code === '62010') {
+      result = (
+        <Text style={{fontSize: 40, textAlign: 'center'}}>
+          Potentiaalinen työnantaja!
+        </Text>
+      );
+    } else if (numberInfo && numberInfo.industry_code === '82200') {
+      result = (
+        <Text style={{fontSize: 40, textAlign: 'center'}}>
+          Pelkkä puhelinmyyjä
+        </Text>
+      );
+    } else {
+      result = (
+        <Text style={{fontSize: 40, textAlign: 'center'}}>
+          Ei tietoa, valitan.{' '}
+        </Text>
+      );
+    }
+
     return (
       <View style={styles.container}>
         <Text style={styles.text}>Laitetaanko numerotutka päälle?</Text>
@@ -146,16 +135,20 @@ export default class App extends React.Component {
             </Text>
           </View>
         </TouchableHighlight>
-        {incoming && <Text style={{fontSize: 20}}>PUHELU {number}</Text>}
-        {loading && <Text style={{fontSize: 20}}>Ladataan tietoja</Text>}
-        {numberInfo && (
-          <Text style={{fontSize: 20}}>{JSON.stringify(numberInfo)}</Text>
+        {incoming && <Text style={{fontSize: 20}}>Call from {number}</Text>}
+        {loading && (
+          <View>
+            <Text style={{fontSize: 20}}>Loading information</Text>
+            <ActivityIndicator size="small" color="greenyellow" />
+          </View>
         )}
-        {error && <Text>Tapahtui virhe</Text>}
+        {numberInfo && result}
+        {incoming && error && <Text>Error occurred</Text>}
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'honeydew',
@@ -168,3 +161,41 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
 });
+
+const mapStateToProps = (state) => {
+  return {
+    featureOn: state.featureOn,
+    incoming: state.incoming,
+    loading: state.loading,
+    number: state.number,
+    numberInfo: state.numberInfo,
+    error: state.error,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setListening: (value) => {
+      dispatch(setListening(value));
+    },
+    incomingCall: (value) => {
+      dispatch(incomingCall(value));
+    },
+    isLoading: (value) => {
+      dispatch(isLoading(value));
+    },
+    getNumber: (value) => {
+      dispatch(getNumber(value));
+    },
+    getInfo: (value) => {
+      dispatch(getInfo(value));
+    },
+    isError: (value) => {
+      dispatch(isError(value));
+    },
+    noCall: (value) => {
+      dispatch(noCall(value));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ keys.js
@@ -0,0 +1,2 @@
+export const apikey =
+  'c8ff45a8c7184c2124faee19b2fc3695c9699ba4819abc132c74d9665375fcc2';
